76def3063490ac4b49790281124ad9366dccc678
jxie5189 <81874035+jxie5189@users.noreply.github.com>
2023-09-29 11:36:46 -0400

    testing security V1

--- code/Employee_management_system/src/main/java/Employee_Management_System/EmsV1Application.java
+++ code/Employee_management_system/src/main/java/Employee_Management_System/EmsV1Application.java
@@ -1,13 +1,44 @@
 
 package Employee_Management_System;
+import Employee_Management_System.credential.CredentialService;
+import Employee_Management_System.credential.RegisterRequest;
+import Employee_Management_System.credential.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmsV1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsV1Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			CredentialService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin1")
+					.lastname("Admin1")
+					.username("user1")
+					.password("password")
+					.role(Role.USER)
+					.build();
+			System.out.println("Admin1 token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin2")
+					.lastname("Admin2")
+					.username("user2")
+					.password("password")
+					.role(Role.ADMIN)
+					.build();
+			System.out.println("Manager1 token: " + service.register(manager).getAccessToken());
+
+		};
 	}
 
 }
--- code/Employee_management_system/src/main/java/Employee_Management_System/credential/CredentialService.java
+++ code/Employee_management_system/src/main/java/Employee_Management_System/credential/CredentialService.java
@@ -1,72 +1,123 @@
 package Employee_Management_System.credential;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import Employee_Management_System.Security.Service.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
+import java.io.IOException;
 
 @Service
-public class CredentialService {
+@RequiredArgsConstructor
+public class CredentialService  {
+    private final CredentialRepository repository;
+    //private final TokenRepository tokenRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
-    @Autowired
-    private CredentialRepository credentialRepository;
-
-    public Credential createAccount(Credential credential) {
-        // Hash the password and save the credential
-        //String hashedPassword = password;
-        String password = credential.getPasswordHash();
-        credential.setPasswordHash(password);
-        return credentialRepository.save(credential);
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = Credential.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+        repository.save(user);
+        //var savedUser = repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        //var refreshToken = jwtService.generateRefreshToken(user);
+        //saveUserToken(savedUser, jwtToken);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                //.refreshToken(refreshToken)
+                .build();
     }
 
-    public Credential login(String username, String password) {
-        Optional<Credential> optionalCredential = credentialRepository.findByUsername(username);
-
-        if(optionalCredential.isPresent()) {
-            Credential credential = optionalCredential.get();
-            if(password.equals(credential.getPasswordHash())) {
-                return credential;
-            }
-        }
-        return null;
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUserName(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByUsername(request.getUserName())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        //var refreshToken = jwtService.generateRefreshToken(user);
+        //revokeAllUserTokens(user);
+        //saveUserToken(user, jwtToken);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                //.refreshToken(refreshToken)
+                .build();
     }
 
-//    // READ
-//    public Credential getCredential(String username) {
-//        return credentialRepository.findById(username)
-//                .orElseThrow(() -> new ResourceNotFoundException("Credential", "username", username));
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        return repository.findByUsername(username);
 //    }
-//
-//    // UPDATE
-//    public Credential updateCredential(String username, Credential credentialDetails) {
-//        Credential credential = getCredential(username); // or use findById(username).orElseThrow(...)
-//        // Set fields that need to be updated
-//        credential.setPasswordHash(credentialDetails.getPasswordHash());
-//        return credentialRepository.save(credential);
+
+//    private void saveUserToken(User user, String jwtToken) {
+//        var token = Token.builder()
+//                .user(user)
+//                .token(jwtToken)
+//                .tokenType(TokenType.BEARER)
+//                .expired(false)
+//                .revoked(false)
+//                .build();
+//        tokenRepository.save(token);
 //    }
-//
-//    // DELETE
-//    public void deleteCredential(String username) {
-//        Credential credential = getCredential(username); // or use findById(username).orElseThrow(...)
-//        credentialRepository.delete(credential);
+
+//    private void revokeAllUserTokens(User user) {
+//        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+//        if (validUserTokens.isEmpty())
+//            return;
+//        validUserTokens.forEach(token -> {
+//            token.setExpired(true);
+//            token.setRevoked(true);
+//        });
+//        tokenRepository.saveAll(validUserTokens);
 //    }
-//
-//    // READ ALL
-//    public List<Credential> getAllCredentials() {
-//        return credentialRepository.findAll();
-//    }
-//
-//    public Credential saveCredential(Credential credential) {
-//        return credentialRepository.save(credential);
-//    }
-//
-//    public Optional<Credential> findCredentialById(String id) {
-//        return credentialRepository.findById(id);
-//    }
-//
-//    public void deleteCredential(Credential credential) {
-//        credentialRepository.delete(credential);
+
+//    public void refreshToken(
+//            HttpServletRequest request,
+//            HttpServletResponse response
+//    ) throws IOException {
+//        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+//        final String refreshToken;
+//        final String userEmail;
+//        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+//            return;
+//        }
+//        refreshToken = authHeader.substring(7);
+//        userEmail = jwtService.extractUsername(refreshToken);
+//        if (userEmail != null) {
+//            var user = this.repository.findByUsername(userEmail)
+//                    .orElseThrow();
+//            if (jwtService.isTokenValid(refreshToken, user)) {
+//                var accessToken = jwtService.generateToken(user);
+//                revokeAllUserTokens(user);
+//                saveUserToken(user, accessToken);
+//                var authResponse = AuthenticationResponse.builder()
+//                        .accessToken(accessToken)
+//                        .refreshToken(refreshToken)
+//                        .build();
+//                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+//            }
+//        }
 //    }
 }
-
